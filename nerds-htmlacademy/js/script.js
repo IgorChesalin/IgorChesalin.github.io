a933ad0e2c34ac8aa9462283e6a4111659214afa
Igor Chesalin <igorchesalin@gmail.com>
2023-10-31 12:21:03 +0100

    add nerd build

---
+++ nerds-htmlacademy/js/script.js
@@ -0,0 +1,29 @@
+// Открытие и закрытие окна
+var link = document.querySelector(".contacts-button");
+var popup = document.querySelector(".modal-content");
+var closelink = document.querySelector(".close-bnt");
+
+link.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+});
+
+closelink.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+});
+
+// Ошибка ввода
+var submitBtn = document.querySelector(".form-button-send");
+var customerName = popup.querySelector(".customer-name");
+var customerEmail = popup.querySelector(".customer-email");
+var customerMessage = popup.querySelector(".customer-text")
+
+submitBtn.addEventListener("click", function (evt) {
+  if (!customerName.value || !customerEmail.value || !customerMessage.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
+});
